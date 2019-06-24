74a77a09d892e9713abf96f7981b3d5b5eb50600
Dwayne Crooks <me@dwaynecrooks.com>
2019-06-24 06:08:40 -0400

    Preview default Markdown

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,122 @@
 module Main exposing (main)
 
 
-import Html exposing (Html, text)
+import Browser
+import Html exposing (Html, div, h2, text, textarea)
+import Html.Attributes exposing (class)
+import Markdown
 
 
-main : Html msg
+main : Program () Model msg
 main =
-  text "Hello, world!"
+  Browser.sandbox
+    { init = init
+    , view = view
+    , update = (\_ model -> model)
+    }
+
+
+-- MODEL
+
+
+type alias Model = String
+
+
+init : Model
+init = defaultContent
+
+
+-- VIEW
+
+
+view : Model -> Html msg
+view content =
+  div []
+    [ viewEditor content
+    , viewPreviewer content
+    ]
+
+
+viewEditor : String -> Html msg
+viewEditor content =
+  div []
+    [ h2 [] [ text "Editor" ]
+    , textarea [ class "editor" ] [ text content ]
+    ]
+
+
+viewPreviewer : String -> Html msg
+viewPreviewer content =
+  div []
+    [ h2 [] [ text "Previewer" ]
+    , toHtml content
+    ]
+
+
+-- HELPERS
+
+
+toHtml : String -> Html msg
+toHtml =
+  Markdown.toHtmlWith
+    { githubFlavored = Just { tables = True, breaks = False }
+    , defaultHighlighting = Just "elm"
+    , sanitize = True
+    , smartypants = False
+    }
+    []
+
+
+-- DATA
+
+
+defaultContent : String
+defaultContent =
+  """# Welcome to my Elm Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, `<div></div>`, between 2 backticks.
+
+```
+-- This is multi-line code:
+
+head : List a -> Maybe a
+head list =
+  case list of
+    [] ->
+      Nothing
+
+    (x:_) ->
+      Just x
+```
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://learn.freecodecamp.org/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer/), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![Elm Logo](https://tinyurl.com/y57bom6e)"""

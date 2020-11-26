7e0bf1121a13e9c01f9c3dc021ec3464abbdad77
Dwayne Crooks <me@dwaynecrooks.com>
2020-11-26 14:35:55 -0400

    Display both windows minimized with no content

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,76 @@
 module Main exposing (main)
 
 
-import Html exposing (Html, text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 main : Html msg
 main =
-  text "Hello, world!"
+  div []
+    [ div [ class "container container--width--small" ]
+        [ viewMinimizedEditorWindow
+        ]
+    , div [ class "container container--width--medium" ]
+        [ viewMinimizedPreviewerWindow
+        ]
+    , footer []
+        [ p [ class "attribution" ]
+            [ text "by "
+            , a
+              [ href "https://www.dwaynecrooks.com" ]
+              [ text "Dwayne Crooks" ]
+            ]
+        ]
+    ]
+
+
+viewMinimizedEditorWindow : Html msg
+viewMinimizedEditorWindow =
+  div [ class "window window--theme--forest" ]
+    [ div [ class "window__frame" ]
+        [ div [ class "window__header" ]
+            [ div [ class "window__icon" ]
+                [ i [ class "fas fa-edit" ] []
+                ]
+            , h2 [ class "window__title" ] [ text "Editor" ]
+            , button [ class "window__button" ]
+                [ i
+                  [ class "fas fa-expand"
+                  , title "Click to maximize"
+                  ]
+                  []
+                ]
+            ]
+        , div [ class "window__body" ]
+            [ textarea [ class "editor window__content" ] []
+            ]
+        ]
+    ]
+
+
+viewMinimizedPreviewerWindow : Html msg
+viewMinimizedPreviewerWindow =
+  div [ class "window window--theme--forest" ]
+    [ div [ class "window__frame" ]
+        [ div [ class "window__header" ]
+            [ div [ class "window__icon" ]
+                [ i [ class "fab fa-html5" ] []
+                ]
+            , h2 [ class "window__title" ] [ text "Previewer" ]
+            , button [ class "window__button" ]
+                [ i
+                  [ class "fas fa-expand"
+                  , title "Click to maximize"
+                  ]
+                  []
+                ]
+            ]
+        , div [ class "window__body" ]
+            [ div [ class "previewer window__content" ]
+                [ div [ class "previewer__html" ]
+                    []
+                ]
+            ]
+        ]
+    ]
